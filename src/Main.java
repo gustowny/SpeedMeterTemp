d616acb1a10bf8b57d3b0d9f08974a98dcf2b4af
gustowny <pawpas1991@gmail.com>
2019-01-04 07:31:20 +0100

    change 3: change  name variable

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,38 @@ public class Main {
             double standardRotation = 0.00198511385;
 
             System.out.println("");
-            System.out.println("INFORMACJA WPROWADZAJĄCA DLA NIKODEMA. Program powie Ci o ile twoja nowa opona będzie Cię okłamywać. Dla opony o symbolach: 205/55 r16:");
+            System.out.println("INFORMACJA WPROWADZAJĄCA DLA NIKODEMA");
+            System.out.println("Program powie Ci o ile twoja nowa opona będzie Cię okłamywać. Dla opony o symbolach: 205/55 r16:");
             System.out.println("205 - oznacza szerokość");
             System.out.println("55 - oznacza wysokość opony w % do szerokości");
             System.out.println("r16 - to rozmiar opony w calach");
             System.out.println("");
-            System.out.println("");
 
             Scanner odczyt = new Scanner(System.in);
-            String[][] daneOpony = {{"szerokosc [mm]", "wysokosc [%]", "rozmiar [cale]"}, {"1", "1", "1"}};
-            int[][] tolerancja = {{150, 25, 14}, {320, 75, 20}};
+            String[][] dataWheel = {{"szerokosc [mm]", "wysokosc [%]", "rozmiar [cale]"}, {"1", "1", "1"}};
+            int[][] tolerance = {{150, 25, 14}, {320, 75, 20}};
 
             for (int i = 0; i < 3; i++) {
-                while (!(Integer.parseInt(daneOpony[1][i]) >= tolerancja[0][i] && Integer.parseInt(daneOpony[1][i]) <= tolerancja[1][i])) {
+                while (!(Integer.parseInt(dataWheel[1][i]) >= tolerance[0][i] && Integer.parseInt(dataWheel[1][i]) <= tolerance[1][i])) {
                     try {
-                        System.out.println("Podaj " + daneOpony[0][i] + " wymarzonej oponki: ");
-                        daneOpony[1][i] = odczyt.next();
+                        System.out.println("Podaj " + dataWheel[0][i] + " wymarzonej oponki: ");
+                        dataWheel[1][i] = odczyt.next();
 
-                        if ((Integer.parseInt(daneOpony[1][i]) < tolerancja[0][i] || Integer.parseInt(daneOpony[1][i]) > tolerancja[1][i])) {
-                            System.out.println("Spróbuj ponownie: Twoja liczba nie mieści się w zakładanym zakresie od " + tolerancja[0][i] + " do " + tolerancja[1][i]);
+                        if ((Integer.parseInt(dataWheel[1][i]) < tolerance[0][i] || Integer.parseInt(dataWheel[1][i]) > tolerance[1][i])) {
+                            System.out.println("Spróbuj ponownie: Twoja liczba nie mieści się w zakładanym zakresie od " + tolerance[0][i] + " do " + tolerance[1][i]);
                         }
 
                     } catch (NumberFormatException n) {
                         System.out.println("MUSISZ PODAĆ LICZBĘ DYWANIKU!");
-                        daneOpony[1][i] = 1 + "1";
+                        dataWheel[1][i] = 1 + "1";
                     }
                 }
             }
-            double newRotation = Math.PI * ((Integer.parseInt(daneOpony[1][2]) * 25.4) + ((Integer.parseInt(daneOpony[1][0]) * Integer.parseInt(daneOpony[1][1]) / 100) * 2)) / 1000000;
+            double newRotation = Math.PI * ((Integer.parseInt(dataWheel[1][2]) * 25.4) + ((Integer.parseInt(dataWheel[1][0]) * Integer.parseInt(dataWheel[1][1]) / 100) * 2)) / 1000000;
             double newQuantityRotationPer100km = 100 / newRotation;
-            double wskazaniePredkosciomierza = newQuantityRotationPer100km * standardRotation;
+            double Speedometer = newQuantityRotationPer100km * standardRotation;
 
-            System.out.println("Gdy będziesz jechał z prędkością 100km/h, twój prędkościomierz pokaże: " + (int) wskazaniePredkosciomierza + "km/h");
+            System.out.println("Gdy będziesz jechał z prędkością 100km/h, twój prędkościomierz pokaże: " + (int) Speedometer + "km/h");
         }
 }
 
